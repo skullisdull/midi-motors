6dcd35fb3bf77b7a54338a1a6654a8ff0e444f3f
Skull_is_dull <booth.w.2005@gmail.com>
2022-06-18 17:27:29 +0100

    Midi Motors blank page

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const favicon = require("serve-favicon");
+const bodyParser = require('body-parser');
 const path = require("path");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "index.html"));
+  res.status(200).sendFile(path.join(__dirname, "public/index.html"));
 });
+
+app.use(express.static("public"));
 
 app.listen(5000, () => {
   console.log("Server is up and on port 5000");
